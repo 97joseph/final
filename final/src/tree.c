ebca44da996e62e39abbb206e300f42d85cb7f4d
97joseph <33089347+97joseph@users.noreply.github.com>
2021-08-07 14:17:11 +0300

    R

--- final/src/tree.c
+++ final/src/tree.c
@@ -5,15 +5,26 @@
 
 #if !defined(GRADE_SUBLINGS)
 int get_highest_path_weight(TreeNode *root)
+int convert_char_to_num(char c) 
 {
-
+	if (c >= '0' && c <= '9')
+    		return c - '0';
+  	else if (c >= 'a' && c <= 'z')
+    		return c - 'a';
+  	else
+    		return -1;  // -1 means invalid
 }
-#endif
 
 #if !defined(GRADE_PATH_WEIGHT)
 int count_siblings(TreeNode *root)
+char convert_num_to_char(int n) 
 {
-
+	if (n >= 0 && n <= 9)
+    		return n + '0';
+  	else if (n >= 10 && n <= 35)
+    		return n + 'a';
+  	else
+    		return 0; // 0 means invalid
 }
 #endif
 ////////////////////////////////////////////////////////////////////////////////
